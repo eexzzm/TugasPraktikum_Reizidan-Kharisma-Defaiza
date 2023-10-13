8bc7a7cf7cead8167629162cec7e41b827f6b7fd
eexzzm <reizidankharisma@gmail.com>
2023-10-13 23:30:29 +0700

    menambahkan file alproCopy.cpp

---
+++ alproCopy.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"-------------------------------------------------------\n";
+    cout<<"Nama: Reizidan Kharisma Defaiza\n\n";
+    cout<<"NPM: 2310631170044\n\n";
+    cout<<"Kelas: B - Informatika\n\n";
+    cout<<"Cita-Cita: Bahagia dunia akhirat\n\n";
+    cout<<"Motivasi Masuk Informatika: Minat\n";
+    cout<<"-------------------------------------------------------";
+    
+    return 0;
+}
